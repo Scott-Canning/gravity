8483bdb6718d978096ce8910efbaa5b98b84e726
Scott-Canning <34758484+Scott-Canning@users.noreply.github.com>
2022-03-09 15:52:24 -0500

    updated front-end

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,20 +2,19 @@ const hre = require("hardhat");
 const { ethers } = require('ethers');
 require('dotenv').config();
 
-// boilerplate placeholder (requires configuration)
-//const sourceToken = ''
-//const targetToken = ''
+const sourceToken = '0xC4375B7De8af5a38a93548eb8453a498222C4fF2'; // DAI
+const targetToken = '0xd0A1E359811322d97991E03f863a0C30C2cF029C'; // WETH
 
 async function deploy() {
   const url = process.env.KOVAN_URL;
   const provider = new ethers.providers.JsonRpcProvider(url);
 
-  let privateKey = process.env.KOVAN_KEY;
+  let privateKey = process.env.PRIVATE_KEY;
   let wallet = new ethers.Wallet(privateKey, provider);
 
   let artifacts = await hre.artifacts.readArtifact("Gravity");
   let factory = new ethers.ContractFactory(artifacts.abi, artifacts.bytecode, wallet);
-  let contract = await factory.deploy(/* sourceToken, targetToken */);
+  let contract = await factory.deploy(sourceToken, targetToken);
 
   console.log("Contract address:", contract.address);
   await contract.deployed();
